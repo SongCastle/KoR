2bb622558863dddd832c254a11a775f0643e1217
rei01saito <rei01saito@gmail.com>
2022-01-25 10:21:02 +0900

    SignupForm修正

--- front/src/components/SignupForm.jsx
+++ front/src/components/SignupForm.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React from 'react';
 
-export const SingupForm = () => {
-  return (
-    <div className="flex justify-center pt-6">
-      <form className="w-1/2 border-2 rounded-lg shadow-sm p-5">
-        <div className="flex flex-col mb-5">
-          <label className="mb-2">Username</label>
-          <input type="text" placeholder="username" className="border-2 rounded pt-2"></input>
-        </div>
-        <div className="flex flex-col mb-5">
-          <label className="mb-2">Email</label>
-          <input type="text" placeholder="email" className="border-2 rounded pt-2"></input>
-        </div>
-        <div className="flex flex-col mb-7">
-          <label className="mb-2">Password</label>
-          <input type="text" placeholder="password" className="border-2 rounded pt-2"></input>
-        </div>
-        <input type="submit" className="bg-purple-400 border-2 rounded-lg px-2 py-1" />
-      </form>
-    </div>
-  );
-};
+export const SingupForm = () => (
+  <div className='flex justify-center pt-6'>
+    <form className='w-1/3 border-2 rounded-lg shadow-sm p-5'>
+      <div className='mb-5'>
+        <label className='flex flex-col mb-2' htmlFor='username'>
+          Username
+          <input
+            id='username'
+            type='text'
+            placeholder='username'
+            className='border-2 rounded p-2'
+          />
+        </label>
+      </div>
+      <div className='mb-5'>
+        <label className='flex flex-col mb-2' htmlFor='email'>
+          Email
+          <input id='email' type='text' placeholder='email' className='border-2 rounded p-2' />
+        </label>
+      </div>
+      <div className='mb-7'>
+        <label className='flex flex-col mb-2' htmlFor='password'>
+          Password
+          <input
+            id='password'
+            type='text'
+            placeholder='password'
+            className='border-2 rounded p-2'
+          />
+        </label>
+      </div>
+      <input type='submit' className='bg-purple-400 border-2 rounded-lg px-2 py-1' />
+    </form>
+  </div>
+);
