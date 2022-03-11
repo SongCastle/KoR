9e76368385ab1495868b5e525d50bca31d0b8e0b
uk <47803499+SongCastle@users.noreply.github.com>
2022-03-12 06:21:26 +0900

    乱数生成方法の変更
    乱数の生成方法を変更しました。
    乱数の強度を高めたいためとなります。
    
    変更前|変更後
    :-:|:-:
    `"math/rand"`<br>(時間を元にした疑似乱数)|"crypto/rand"`<br>(ハードのノイズ ? などを元にした疑似乱数)

--- back/src/internal/random/random.go
+++ back/src/internal/random/random.go
@@ -1,18 +1,37 @@
 package random
 
 import (
-	// TODO: "crypto/rand" にする
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"encoding/base64"
+	mrand "math/rand"
 	"strconv"
+	"time"
 )
 
+var Encoder *base64.Encoding = base64.RawStdEncoding
+
 func Generate(len int) string {
-	rand.Seed(time.Now().UnixNano())
+	if r, err := generateStrongly(len); err == nil {
+		return r
+	}
+	return generateWeakly(len)
+}
+
+func generateStrongly(len int) (string, error) {
+	b := make([]byte, len)
+	_, err := rand.Read(b)
+	if err == nil {
+		return Encoder.EncodeToString(b)[:len], nil
+	}
+	return "", err
+}
+
+func generateWeakly(len int) string {
+	mrand.Seed(time.Now().UnixNano())
 
 	word := ""
 	for i := 0; i < len; i++ {
-		n := rand.Intn(62)
+		n := mrand.Intn(62)
 		if n < 26 {
 			// 小文字
 			word += string('a' + n)
