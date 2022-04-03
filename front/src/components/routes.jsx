984efc3336e1369e82b6997217c319e1066976f5
uk <47803499+SongCastle@users.noreply.github.com>
2022-04-03 16:29:03 +0900

    react-slick サンプル
    react-slick の利用サンプルを作成しました。
    > http://0.0.0.0:8080/slick
    
    slick-carousel を import する箇所は、 jsx ファイル内ではなく、 css ファイルの方が正しいかもです ..

--- front/src/components/routes.jsx
+++ front/src/components/routes.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Top } from './Top';
 import { Users } from './Users';
+import { Slick } from './Slick';
 
 export const Routes = () => (
   <Router>
     <Switch>
       <Route exact path='/users' component={Users} />
+      <Route exact path='/slick' component={Slick} />
       <Route path='/' component={Top} />
     </Switch>
   </Router>
