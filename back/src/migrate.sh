b60155baa64133fbbd99b8c67fc9881b629176f6
uk <47803499+SongCastle@users.noreply.github.com>
2024-09-21 15:43:23 +0900

    [back] migrate.sh で参照するパスワードを MYSQL_USER のものにする
    MYSQL_USER としてマイグレーションを実行するため。

--- back/src/migrate.sh
+++ back/src/migrate.sh
@@ -3,7 +3,7 @@
 cmd=$1
 v=$2
 
-pass=$(cat $MYSQL_ROOT_PASSWORD_FILE)
+pass=$(cat $MYSQL_PASSWORD_FILE)
 db="mysql://$MYSQL_USER:$pass@tcp($MYSQL_HOST:$MYSQL_PORT)/$MYSQL_DATABASE?charset=utf8mb4&parseTime=True&loc=Local&multiStatements=True"
 
 if [ -z "$v" ]; then
